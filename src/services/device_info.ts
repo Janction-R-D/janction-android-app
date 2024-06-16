ecdfed57de161640d6ac6df75cc245d5cb874cdc
GretchenLeee <lyf1990s@gmail.com>
2024-06-16 12:04:32 +0800

    chore: change api url

--- src/services/device_info.ts
+++ src/services/device_info.ts
@@ -2,7 +2,7 @@ import {ParamCreateDeviceInfo} from '../types';
 import {http} from '../utils';
 
 export const fetchDeviceInfo = async (wallet_address?: string) => {
-  await http.get('device_info', {
+  await http.get('device/info', {
     params: {
       wallet_address,
     },
@@ -10,7 +10,7 @@ export const fetchDeviceInfo = async (wallet_address?: string) => {
 };
 
 export const submitDeviceInfo = async (token: string, params: ParamCreateDeviceInfo) => {
-  return await http.post('device_info', params, {
+  return await http.post('device/info', params, {
     headers: {
       'Content-Type': 'application/json',
       'Authorization': `Bearer ${token}` 
--- src/services/login.ts
+++ src/services/login.ts
@@ -2,12 +2,12 @@ import {ParamLogin} from '../types';
 import {http} from '../utils';
 
 export const fetchNonce = async () => {
-  const res = await http.get('nonce');
+  const res = await http.get('auth/nonce');
   return res.data.nonce;
 };
 
 export const login = async (params: ParamLogin) => {
-  const res = await http.post('login', params, {
+  const res = await http.post('auth/login', params, {
     headers: {
       'Content-Type': 'application/json',
     },
